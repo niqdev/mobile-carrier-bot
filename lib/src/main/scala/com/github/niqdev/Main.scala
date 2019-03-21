04d0bb4a920b857e0c52d4e0efd5d2d5203b0dd7
niqdev <niqdev@gmail.com>
2019-03-21 10:44:35 +0000

    refactor settings

--- lib/src/main/scala/com/github/niqdev/Main.scala
+++ lib/src/main/scala/com/github/niqdev/Main.scala
@@ -2,11 +2,7 @@ package com.github.niqdev
 
 import cats.effect.{ExitCode, IO, IOApp, Sync}
 import cats.implicits.{catsSyntaxApply, toFlatMapOps, toFunctorOps}
-import ciris.cats.catsMonadErrorToCiris
-import ciris.refined.refTypeConfigDecoder
-import ciris.{envF, loadConfig}
 import com.github.niqdev.model.Settings
-import eu.timepit.refined.types.string.NonEmptyString
 import io.chrisdavenport.log4cats.Logger
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 
@@ -15,18 +11,19 @@ object Main extends IOApp {
   private[this] def program[F[_] : Sync](log: Logger[F]): F[Unit] =
     for {
       _ <- log.info("Hello World")
-      settings <- loadConfig(
-        envF[F, NonEmptyString]("ENVIRONMENT")
-      )(Settings.apply).orRaiseThrowable
+      settings <- Settings.load[F]
       _ <- log.info(s"$settings")
     } yield ()
-
-  private[this] def success[F[_] : Sync](log: Logger[F]): Unit => F[ExitCode] =
-    _ => log.info("Application succeeded") *> Sync[F].pure(ExitCode.Success)
 
   private[this] def error[F[_] : Sync](log: Logger[F])(e: Throwable): F[ExitCode] =
     log.error(e)("Application failed") *> Sync[F].pure(ExitCode.Error)
 
+  private[this] def success[F[_] : Sync](log: Logger[F]): Unit => F[ExitCode] =
+    _ => log.info("Application succeeded") *> Sync[F].pure(ExitCode.Success)
+
+  /**
+    *
+    */
   override def run(args: List[String]): IO[ExitCode] =
     Slf4jLogger.create[IO].flatMap(log =>
       program[IO](log).redeemWith(error[IO](log), success[IO](log)))
--- lib/src/main/scala/com/github/niqdev/model/Settings.scala
+++ lib/src/main/scala/com/github/niqdev/model/Settings.scala
@@ -1,6 +1,19 @@
 package com.github.niqdev
 package model
 
+import cats.effect.Sync
+import ciris.cats.catsMonadErrorToCiris
+import ciris.refined.refTypeConfigDecoder
+import ciris.{envF, loadConfig}
 import eu.timepit.refined.types.string.NonEmptyString
 
 case class Settings(environment: NonEmptyString)
+
+object Settings {
+
+  def load[F[_] : Sync]: F[Settings] =
+    loadConfig(
+      envF[F, NonEmptyString]("ENVIRONMENT")
+    )(Settings.apply).orRaiseThrowable
+
+}
