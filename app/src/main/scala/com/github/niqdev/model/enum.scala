f37987d6db812770ec5391b84c675c68ff0dc834
niqdev <niqdev@gmail.com>
2019-04-29 14:27:36 +0100

    add logLevel enum

--- app/src/main/scala/com/github/niqdev/model/enum.scala
+++ app/src/main/scala/com/github/niqdev/model/enum.scala
@@ -5,6 +5,9 @@ import enumeratum.EnumEntry.{ Snakecase, Uppercase }
 import enumeratum.{ Enum, EnumEntry }
 import io.circe.Encoder
 
+/**
+  *
+  */
 sealed trait Environment extends EnumEntry with Snakecase with Uppercase
 
 sealed trait EnvironmentInstances {
@@ -21,5 +24,28 @@ object Environment extends Enum[Environment] with EnvironmentInstances {
   case object Local       extends Environment
   case object Development extends Environment
   case object Production  extends Environment
+
+}
+
+/**
+  * [[org.slf4j.event.Level LogLevel]]
+  */
+sealed trait LogLevel extends EnumEntry with Snakecase with Uppercase
+
+sealed trait LogLevelInstances {
+
+  implicit val encodeLogLevel: Encoder[LogLevel] =
+    Encoder.encodeString.contramap[LogLevel](_.entryName)
+}
+
+object LogLevel extends Enum[LogLevel] with LogLevelInstances {
+
+  // macro
+  val values = findValues
+
+  case object Debug extends LogLevel
+  case object Info  extends LogLevel
+  case object Warn  extends LogLevel
+  case object Error extends LogLevel
 
 }
