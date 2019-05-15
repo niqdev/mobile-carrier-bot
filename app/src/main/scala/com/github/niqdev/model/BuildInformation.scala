7168ccca92d3d8326d7b217cc2f191fc9f245bbb
niqdev <niqdev@gmail.com>
2019-05-15 19:20:13 +0100

    add infoEndpoint test

--- app/src/main/scala/com/github/niqdev/model/BuildInformation.scala
+++ app/src/main/scala/com/github/niqdev/model/BuildInformation.scala
@@ -2,10 +2,11 @@ package com.github.niqdev
 package model
 
 import cats.Applicative
-import io.circe.Encoder
-import io.circe.generic.semiauto.deriveEncoder
-import org.http4s.EntityEncoder
-import org.http4s.circe.jsonEncoderOf
+import cats.effect.Sync
+import io.circe.generic.semiauto.{ deriveDecoder, deriveEncoder }
+import io.circe.{ Decoder, Encoder }
+import org.http4s.circe.{ jsonEncoderOf, jsonOf }
+import org.http4s.{ EntityDecoder, EntityEncoder }
 
 final case class BuildInformation(name: String, version: String, scalaVersion: String, buildTime: String)
 
@@ -16,4 +17,11 @@ object BuildInformation {
 
   implicit def buildInformationEntityEncoder[F[_]: Applicative]: EntityEncoder[F, BuildInformation] =
     jsonEncoderOf[F, BuildInformation]
+
+  implicit val buildInformationDecoder: Decoder[BuildInformation] =
+    deriveDecoder[BuildInformation]
+
+  implicit def buildInformationEntityDecoder[F[_]: Sync]: EntityDecoder[F, BuildInformation] =
+    jsonOf[F, BuildInformation]
+
 }
--- app/src/test/scala/com/github/niqdev/http/HealthCheckEndpointsSpec.scala
+++ app/src/test/scala/com/github/niqdev/http/HealthCheckEndpointsSpec.scala
@@ -4,18 +4,21 @@ package http
 import java.nio.charset.StandardCharsets
 
 import cats.effect.IO
-import org.http4s.{Method, Request, Status, Uri}
+import com.github.niqdev.model.BuildInformation
+import com.github.niqdev.service.HealthCheckService
+import org.http4s.{ Method, Request, Status, Uri }
 
-// https://github.com/jaspervz/todo-http4s-doobie/blob/master/src/test/scala/service/TodoServiceSpec.scala
-// https://github.com/jaspervz/todo-http4s-doobie/blob/master/src/it/scala/TodoServerSpec.scala
-// https://github.com/barambani/http4s-extend/blob/master/src/main/scala/http4s/extend/syntax/ResponseVerificationSyntax.scala#L40
 final class HealthCheckEndpointsSpec extends BaseSpec {
 
   "HealthCheckEndpoints" must {
 
     "verify statusEndpoint" in {
       val request = Request[IO](method = Method.GET, uri = Uri.unsafeFromString("/status"))
-      val maybeResponse = HealthCheckEndpoints[IO].statusEndpoint.run(request).value.unsafeRunSync()
+      val maybeResponse = HealthCheckEndpoints[IO]
+        .statusEndpoint
+        .run(request)
+        .value
+        .unsafeRunSync
 
       maybeResponse match {
         case Some(response) =>
@@ -23,6 +26,34 @@ final class HealthCheckEndpointsSpec extends BaseSpec {
 
           val byteArray = response.body.compile.toVector.unsafeRunSync().toArray
           new String(byteArray, StandardCharsets.UTF_8) shouldBe Status.Ok.reason
+        case _ =>
+          fail("unexpected failure")
+      }
+    }
+
+    "verify infoEndpoint" in {
+      val request = Request[IO](method = Method.GET, uri = Uri.unsafeFromString("/info"))
+      val maybeResponse = HealthCheckEndpoints[IO]
+        .infoEndpoint(HealthCheckService())
+        .run(request)
+        .value
+        .unsafeRunSync
+
+      maybeResponse match {
+        case Some(response) =>
+          response.status shouldBe Status.Ok
+
+          response.attemptAs[BuildInformation].value.unsafeRunSync() match {
+            case Right(buildInformation) =>
+              buildInformation.name shouldBe "mobile-carrier-bot"
+              buildInformation.scalaVersion shouldBe "2.12.8"
+
+              buildInformation.version shouldBe "0.1"
+              // e.g. 2019-05-15 18:17:03.286
+              buildInformation.buildTime.nonEmpty shouldBe true
+            case _ =>
+              fail("unexpected failure")
+          }
         case _ =>
           fail("unexpected failure")
       }
