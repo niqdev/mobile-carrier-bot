a064ea6f679eb0b6fd7985e647c75b9a5605feb8
niqdev <niqdev@gmail.com>
2019-05-15 18:58:10 +0100

    draft statusEndpoint test

--- app/src/test/scala/com/github/niqdev/http/HealthCheckEndpointsSpec.scala
+++ app/src/test/scala/com/github/niqdev/http/HealthCheckEndpointsSpec.scala
@@ -1,13 +1,32 @@
 package com.github.niqdev
 package http
 
+import java.nio.charset.StandardCharsets
+
+import cats.effect.IO
+import org.http4s.{Method, Request, Status, Uri}
+
 // https://github.com/jaspervz/todo-http4s-doobie/blob/master/src/test/scala/service/TodoServiceSpec.scala
 // https://github.com/jaspervz/todo-http4s-doobie/blob/master/src/it/scala/TodoServerSpec.scala
+// https://github.com/barambani/http4s-extend/blob/master/src/main/scala/http4s/extend/syntax/ResponseVerificationSyntax.scala#L40
 final class HealthCheckEndpointsSpec extends BaseSpec {
 
   "HealthCheckEndpoints" must {
 
-    "TODO" in {}
+    "verify statusEndpoint" in {
+      val request = Request[IO](method = Method.GET, uri = Uri.unsafeFromString("/status"))
+      val maybeResponse = HealthCheckEndpoints[IO].statusEndpoint.run(request).value.unsafeRunSync()
+
+      maybeResponse match {
+        case Some(response) =>
+          response.status shouldBe Status.Ok
+
+          val byteArray = response.body.compile.toVector.unsafeRunSync().toArray
+          new String(byteArray, StandardCharsets.UTF_8) shouldBe Status.Ok.reason
+        case _ =>
+          fail("unexpected failure")
+      }
+    }
   }
 
 }
