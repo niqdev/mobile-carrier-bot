a62f0b6146f22ea74134f617c8a7e429c1415ae0
niqdev <niqdev@gmail.com>
2019-05-18 11:33:41 +0100

    add enums codec tests

--- app/src/test/scala/com/github/niqdev/model/EnumsSpec.scala
+++ app/src/test/scala/com/github/niqdev/model/EnumsSpec.scala
@@ -1,6 +1,21 @@
 package com.github.niqdev
 package model
 
+import io.circe.generic.semiauto.{ deriveDecoder, deriveEncoder }
+import io.circe.{ Decoder, Encoder }
+
+private[this] final case class ExampleAuto(environment: Environment)
+private[this] final case class ExampleSemiAuto(driver: DatabaseDriver)
+
+private[this] object ExampleSemiAuto {
+
+  implicit val exampleEncoder: Encoder[ExampleSemiAuto] =
+    deriveEncoder[ExampleSemiAuto]
+
+  implicit val exampleDecoder: Decoder[ExampleSemiAuto] =
+    deriveDecoder[ExampleSemiAuto]
+}
+
 final class EnumsSpec extends BaseSpec {
 
   "Environment" must {
@@ -11,6 +26,22 @@ final class EnumsSpec extends BaseSpec {
       Environment.Local.entryName shouldBe "LOCAL"
       Environment.Development.entryName shouldBe "DEVELOPMENT"
       Environment.Production.entryName shouldBe "PRODUCTION"
+    }
+
+    "verify encoder" in {
+      import io.circe.generic.auto.exportEncoder
+      import io.circe.syntax.EncoderOps
+
+      val expectedResult = """{"environment":"LOCAL"}"""
+      ExampleAuto(Environment.Local).asJson.noSpaces shouldBe expectedResult
+    }
+
+    "verify decoder" in {
+      import io.circe.generic.auto.exportDecoder
+      import io.circe.parser.decode
+
+      val expectedResult = ExampleAuto(Environment.Development)
+      decode[ExampleAuto]("""{"environment":"DEVELOPMENT"}""") shouldBe Right(expectedResult)
     }
   }
 
@@ -39,6 +70,20 @@ final class EnumsSpec extends BaseSpec {
 
       DatabaseDriver.Cache.entryName shouldBe "cache"
       DatabaseDriver.Cache.className.value shouldBe "cats.effect.concurrent.Ref"
+    }
+
+    "verify encoder" in {
+      import io.circe.syntax.EncoderOps
+
+      val expectedResult = """{"driver":"org.postgresql.Driver"}"""
+      ExampleSemiAuto(DatabaseDriver.PostgreSQL).asJson.noSpaces shouldBe expectedResult
+    }
+
+    "verify decoder" in {
+      import io.circe.parser.decode
+
+      val expectedResult = ExampleSemiAuto(DatabaseDriver.Cache)
+      decode[ExampleSemiAuto]("""{"driver":"cats.effect.concurrent.Ref"}""") shouldBe Right(expectedResult)
     }
   }
 
